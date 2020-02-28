25b51fc510bcaab14d009d57bcd3414220ab6b3e
CarolineRodrigues <c_costa_rodrigues@hotmail.com>
2020-02-28 11:40:43 +0000

    selectors memoized

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-signup.
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.action';
-
+import { selectCurrentUser } from './redux/user/user.selector';
+import {createStructuredSelector} from 'reselect';
 class App extends Component {
 
   unsubscribeFromAuth = null;
@@ -52,8 +53,8 @@ class App extends Component {
 
   }
 }
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector ({
+  currentUser: selectCurrentUser
 })
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
