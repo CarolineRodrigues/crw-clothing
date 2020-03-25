2277b62b2a181d2af8639d5506cdce395d28ed45
CarolineRodrigues <c_costa_rodrigues@hotmail.com>
2020-03-25 17:09:47 +0000

    backend 02

---
+++ client/build/precache-manifest.801d7f999d86341a2959710fad898def.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df59e4b54657eced40f554339ed0bac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a89aa43e824c7af4495a",
+    "url": "/static/css/main.c54fd649.chunk.css"
+  },
+  {
+    "revision": "793bf1fe6dfed3a54289",
+    "url": "/static/js/2.aab453c1.chunk.js"
+  },
+  {
+    "revision": "4c86fd10a56eb60a384cd2f2c8426c35",
+    "url": "/static/js/2.aab453c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89aa43e824c7af4495a",
+    "url": "/static/js/main.361cdf33.chunk.js"
+  },
+  {
+    "revision": "df6638462767c6c7e6aa",
+    "url": "/static/js/runtime-main.43bb2de7.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
